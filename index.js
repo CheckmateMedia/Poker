e4799e5287b045861725a6f2977377734105faf1
Checkmate Media <34839284+CheckmateMedia@users.noreply.github.com>
2020-07-16 11:58:08 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,49 @@
+function buttonClick() {
+    //Show
+    document.getElementById("playCards").style.display = "block";
+    //Hide
+    document.getElementById("deal").style.display = "none";
+    document.getElementById("deal").style.display = "none";
+    document.getElementById("title").style.display = "none";
+    document.getElementById("ante").style.display = "none";
+    document.getElementById("table").style.display = "none";
+    document.getElementById("input").style.display = "none";
+    document.getElementById("enter").style.display = "none";
+    document.getElementById("input").style.display = "none";
+}
+
+function newHand() {
+    //Show
+    //not showing deal
+    document.getElementById("deal").style.display = "inline-block";
+    document.getElementById("deal").style.position = "relative";
+
+
+    //works
+    document.getElementById("table").style.display = "block";
+    document.getElementById("table").style.position = "absolute";
+
+    //Hide
+    document.getElementById("enter").style.display = "none";
+    document.getElementById("deal").style.display = "none";
+    document.getElementById("ante").style.display = "none";
+    document.getElementById("title").style.display = "none";
+    document.getElementsByClassName("cards")[0].style.display = "none";
+    document.getElementById("done").style.display = "none";
+
+
+}
+
+function hideBanner() {
+    //Hides
+    document.getElementById("closeButton").style.display = "none";
+    document.getElementById("bannerText").style.display = "none";
+    document.getElementById("bannerText").style.display = "none";
+    document.getElementById("main").style.display = "none";
+    //Shows
+    document.getElementsByClassName("cards")[0].style.display = "block";
+}
+
+function submit() {
+
+}
