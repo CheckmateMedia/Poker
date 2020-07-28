f490d39931ffe6a100eb9a45930771044e7b63e6
Checkmate Media <34839284+CheckmateMedia@users.noreply.github.com>
2020-07-28 10:52:15 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 function deal() {
+    //Hide
+    document.getElementById("intro").style.display = 'none'
     document.getElementById("two").style.display = 'none'
+    //Show
     document.getElementById("one").style.display = 'block'
 }
 
 function newHand() {
+    //Show
     document.getElementById("two").style.display = 'block'
+    //Hide
     document.getElementById("one").style.display = 'none'
+    document.getElementById("intro").style.display = 'none'
 }
